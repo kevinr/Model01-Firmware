51faad6527eaca938d403dc4e63ca170a1c9b8a7
Kevin Riggle <kevinr@Kevins-MBP.local>
2020-04-16 00:37:44 -0700

    remove numpad; mirror hyphen, five, and six

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -177,14 +177,14 @@ KEYMAPS(
 
 #if defined (PRIMARY_KEYMAP_QWERTY)
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, ___,
+  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_6 /*dup*/,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftAlt, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   ___,           Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+   Key_5 /*dup*/, Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Minus /*dup*/,
    ___,           Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_LeftGui,   Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
